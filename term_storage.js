da44bc0909be5ad7c77a5d8605038ba01e566e57
Dhruv Maniyaa <71778799+midnightmadwalk@users.noreply.github.com>
2023-08-31 11:50:26 +0530

    Add files via upload

---
+++ term_storage.js
@@ -0,0 +1,38 @@
+<div className='main'>
+        <div className='swapPage'>
+          <div className='swapTop'>
+            <div>Swap</div>
+          </div>
+          <div className='swapForm'> {/* parental form element - Position*/}
+            <div className='swapBorder'> {/* border and style */}
+              <div className='swapBorderPosition'> {/* position between input and button */}
+                <input className='swapInput' name='firstToken' type='number' placeholder='0'/> {/*maybe reactjs*/}
+                <button className='tokenButton'></button>
+              </div>
+              <div className='swapBorderPrice'>
+
+              </div>
+            </div>
+          </div>
+          <div className='swapForm'> {/* parental form element - Position*/}
+            <div className='swapBorder'>
+            <div className='swapBorderPosition'> {/* position between input and button */}
+                <input className='swapInput' name='firstToken' type='number' placeholder='0'/> {/*maybe reactjs*/}
+                <button className='tokenButton'></button>
+              </div>
+              <div className='swapBorderPrice'>
+        
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
